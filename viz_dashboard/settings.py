68b448dfbe895928eea19d1aabaafff5681d9888
davidhedderman <davidheddermancodes@gmail.com>
2021-02-11 12:15:03 +0000

    added postgres settings

--- viz_dashboard/settings.py
+++ viz_dashboard/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'viz_dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
     }
 }
 
