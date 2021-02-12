18da1d7b4b799323a94f049e204de55d7c36189a
davidhedderman <davidheddermancodes@gmail.com>
2021-02-12 10:14:21 +0000

    changed JSONField as suggested

--- apps/charts/models.py
+++ apps/charts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import JSONField
 
-# Create your models here.
+
+VIZUALISATIONS = (
+    # add chartjs charts allowed below
+    ("bar_chart", "Bar chart"),
+)
+
+
+class Chart(models.Model):
+    name = models.CharField(
+        max_length=120,
+        default="Main dashboard",
+        help_text="Name for the dashboard",
+    )
+    description = models.CharField(
+        max_length=255,
+        help_text="",
+    )
+    viz_type = models.CharField(
+        max_length=20,
+        choices=VIZUALISATIONS,
+        default='bar_chart',
+        help_text="The type of chartjs chart used",
+    )
+    # datasource = TODO
+    params = JSONField(default=dict)
+    # owners = TODO
--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Dashboard(models.Model):
+    name = models.CharField(
+        max_length=120,
+        default="Main dashboard",
+        help_text="Name for the dashboard",
+    )
+    description = models.CharField(
+        max_length=120,
+        default="Main dashboard",
+        help_text="Name for the dashboard",
+    )
+    # owners = TODO
+    # charts = TODO
+    data_up_to_dt = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="What date is the latest data.",
+        verbose_name="Latest data date",
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text="",
+    )
